b3e0516c4ae68a3bbac45e7f5f30ef44e6cf9752
Tianyu Dai <td115@duke.edu>
2018-09-17 12:44:53 -0400

    add examples

---
+++ LLBT_test/Langevin_Boltzmann.cc
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <time.h>
+
+// JetScape Framework includes ...
+#include "JetScape.h"
+#include "JetEnergyLoss.h"
+#include "JetEnergyLossManager.h"
+#include "JetScapeWriterStream.h"
+#ifdef USE_HEPMC
+#include "JetScapeWriterHepMC.h"
+#endif
+
+
+// User modules derived from jetscape framework clasess
+#include "TrentoInitial.h"
+#include "LLBT.h"
+#include "Langevin.h"
+#include "Martini.h"
+#include "Brick.h"
+#include "PGun.h"
+#include "PythiaGun.h"
+#include "PartonPrinter.h"
+
+#include "tinyxml2.h"
+#include <chrono>
+#include <thread>
+
+#define hbarc 0.197327053
+
+using namespace std;
+
+using namespace Jetscape;
+
+// Forward declaration
+void Show();
+
+// -------------------------------------
+
+void RunEvents(double scale, double alpha_s, double omegacut, int N)
+{
+	//modify the init.xml file
+  	  JetScapeXML::Instance()->OpenXMLFile("./Langevin_Boltzmann.xml");
+  	  tinyxml2::XMLElement *scalexml=JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("Eloss" )->FirstChildElement("LLBT" )->FirstChildElement("mu_scale"); 
+  	  scalexml->SetText(scale);
+  	  tinyxml2::XMLElement *alphaxml= JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("Eloss" )->FirstChildElement("LLBT" )->FirstChildElement("alpha_s");
+  	  alphaxml->SetText(alpha_s);
+  	  tinyxml2::XMLElement *omegaxml= JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("Eloss" )->FirstChildElement("LLBT" )->FirstChildElement("omega_over_T_cutoff");
+  	  omegaxml->SetText(omegacut); 
+  	  double t = 0.3*0.3/alpha_s/alpha_s; 
+  	  tinyxml2::XMLElement *lenthxml= JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("Eloss" )->FirstChildElement("maxT" );
+  	  lenthxml->SetText(t);
+	  // JetScapeXML::Instance()->CloseXMLFile();
+	  
+	  double deltaT = 0.1; 
+	  if (scale <= 0.5 || omegacut < 0.5) deltaT = 0.01; 
+      tinyxml2::XMLElement *dtxml= JetScapeXML::Instance()->GetXMLRoot()->FirstChildElement("Eloss" )->FirstChildElement("deltaT" );
+  	  dtxml->SetText(deltaT);
+  	  
+	  auto jetscape = make_shared<JetScape>("./Langevin_Boltzmann.xml",N);
+	  jetscape->SetId("primary");
+
+	  auto trento = make_shared<TrentoInitial>();
+      auto pythiaGun= make_shared<PythiaGun> ();
+	  auto pGun= make_shared<PGun> ();
+	  auto hydro = make_shared<Brick> ();
+	  // jetscape->Add(trento);
+	  // jetscape->Add(pythiaGun);
+	  jetscape->Add(pGun); 
+	  jetscape->Add(hydro); 
+
+
+	  // Energy loss
+	  auto jlossmanager = make_shared<JetEnergyLossManager> ();
+	  auto jloss = make_shared<JetEnergyLoss> ();
+
+	  auto llbt = make_shared<LLBT> ();
+	  jloss->Add(llbt);
+	  // auto martini = make_shared<Martini> ();
+	  // jloss->Add(martini);
+	  // auto langevin = make_shared<Langevin> (); 
+	  // jloss->Add(langevin); 
+	  jlossmanager->Add(jloss);  
+	  jetscape->Add(jlossmanager);
+
+	  auto printer = make_shared<PartonPrinter> ();
+	  jetscape->Add(printer);
+	  
+	  // Output
+	  auto writer= make_shared<JetScapeWriterAscii> (("20GeV_muscale"+std::to_string(scale)+"alpha"+std::to_string(alpha_s)+"omega"+std::to_string(omegacut)+"_gluemedium_inel.dat").c_str());
+	  jetscape->Add(writer); 
+	  
+	  jetscape->Init();
+
+	  jetscape->Exec();
+
+	  jetscape->Finish();
+	  
+	  writer->Close(); 
+}
+
+int main(int argc, char** argv)
+{
+  clock_t t; t = clock();
+  time_t start, end; time(&start);
+
+  JetScapeLogger::Instance()->SetInfo(true);
+  JetScapeLogger::Instance()->SetDebug(false);
+  JetScapeLogger::Instance()->SetRemark(false);
+  JetScapeLogger::Instance()->SetVerboseLevel(0);
+  
+  Show();
+
+  cout<<endl;
+    
+  double scale_list[1] = {1.}; 
+  double alpha_list[1] = {0.3}; 
+  double omegacut_list[6] = {0.1, 0.25, 0.5, 1., 2., 4.}; 
+  for (int i = 0; i < 1; i++)
+  {
+  	  
+	  double scale = scale_list[i]; 
+	  for (int j = 0; j < 1; j++)
+	  {
+	  	double alpha_s = alpha_list[j]; 
+	  	for (int k = 0; k < 6; k++)
+	  	{
+	  		double omegacut = omegacut_list[k]; 
+	  		RunEvents(scale, alpha_s, omegacut, 1000000); 
+	  	}
+	  }
+  }
+  
+  INFO_NICE<<"Finished!"; 
+  cout<<endl;
+
+  t = clock() - t;
+  time(&end);
+  printf ("CPU time: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+  printf ("Real time: %f seconds.\n",difftime(end,start));
+
+  return 0;
+}
+
+// -------------------------------------
+
+void Show()
+{
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE<<"| Brick Test JetScape Framework ... |";
+  INFO_NICE<<"------------------------------------";
+  INFO_NICE;
+}
---
+++ LLBT_test/pureglue.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <chrono>
+#include <thread>
+
+#include "gzstream.h"
+#include "PartonShower.h"
+#include "JetScapeLogger.h"
+#include "JetScapePartonReader.h"
+#include "JetScapeBanner.h"
+#include "fjcore.hh"
+
+#include <GTL/dfs.h>
+
+using namespace std;
+using namespace fjcore;
+
+using namespace Jetscape;
+
+// You could overload here and then simply use ofstream << p;
+// ostream & operator<<(ostream & ostr, const fjcore::PseudoJet & jet);
+
+
+// -------------------------------------
+
+int main(int argc, char** argv)
+{
+  double scale_list[1] = {1.}; 
+  double alpha_list[1] = {0.3}; 
+  double omegacut_list[6] = {0.1, 0.25, 0.5, 1., 2., 4.}; 
+
+  for (int i = 0; i < 1; i++)
+  {
+  	  
+	  double scale = scale_list[i]; 
+	  for (int j = 0; j < 1; j++)
+	  {
+	  	double alpha_s = alpha_list[j]; 
+		for (int k = 0; k < 6; k++)
+		{
+		  double omegacut = omegacut_list[k]; 
+		  auto reader=make_shared<JetScapeReaderAscii>("20GeV_muscale"+std::to_string(scale)+"alpha"+std::to_string(alpha_s)+"omega"+std::to_string(omegacut)+"_gluemedium_inel.dat"); 
+		  std::ofstream jet_output (("../../Result/LLBT/inel/20GeV_muscale"+std::to_string(scale)+"alpha"+std::to_string(alpha_s)+"omega"+std::to_string(omegacut)+"_gluemedium_inel.txt").c_str()); 
+		  const int nEvents = 1000000; 
+		  double Emean = 0.; 
+		  double jetpTMin = 4., jetRadius = 0.4; 
+		  int nParticles = 0; 
+		  fjcore::JetDefinition jetDef(fjcore::antikt_algorithm, jetRadius); 
+		  vector <fjcore::PseudoJet> fjInputs; 
+		  
+		  vector <double> EBin(101); 
+		  for (unsigned int j = 0; j < EBin.size(); j++)
+		  	EBin[j] = 2.+(double) j * 22. / 100.; 
+		  vector <double> cs(EBin.size()-1, 0.), err(EBin.size()-1, 0.); 
+		  vector <int> sqSum(EBin.size()-1, 0); 
+		  
+		  while (!reader->Finished())
+			{
+			  reader->Next();
+			  vector <int> ct(EBin.size()-1, 0); 
+			  vector <shared_ptr <Parton> > partons; 
+			  partons = reader->GetPartons(); 
+			  nParticles += partons.size(); 
+			  for (unsigned int i = 0; i < partons.size(); i++)
+			  {
+			  		double energy = partons[i]->e(); 
+			  		Emean += energy; 
+			  		for (unsigned int j = 0; j < EBin.size() - 1; j++)
+			  			if (energy>=EBin[j] && energy<EBin[j+1])
+						{
+							ct[j]++; 
+							break; 
+						}
+    			}
+			  for (unsigned int j = 0; j < EBin.size() - 1; j++)
+			   {
+		  	   cs[j] += ct[j]; 
+			   sqSum[j] += pow(ct[j], 2);
+			   }
+			 }
+		  Emean /= nParticles; 
+		  // cout<<"The number of particles is "<<nParticles<<endl;
+		  cout<<"The average energy of the final jet is "<<Emean<<endl; 
+		  cout<<"Finished!"<<endl; 
+		  for (unsigned int j=0; j<EBin.size()-1; j++)
+		  {
+			cs[j] /= nEvents; 
+			err[j] = sqrt(((double)sqSum[j]/nEvents - pow(cs[j], 2)) / nEvents); 
+			jet_output << (EBin[j] + EBin[j+1]) / 2 << " " << cs[j] / (EBin[j+1] - EBin[j]) << " " << err[j] / (EBin[j+1] - EBin[j]) << endl; 
+		  }
+		  reader->Close(); 
+		}
+	}
+  }
+}
